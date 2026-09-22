a9cc1004e5841faa1abded886b6e6b25fd493436
Quân <nnquan2000@gmail.com>
2026-09-22 09:27:23 +0700

    xong bài 5

---
+++ Assigment_1/exercise5.py
@@ -0,0 +1,11 @@
+n1 = int(input('nhap n1:'))
+n2 = int(input('nhap n2:'))
+n3 = int(input('nhap n3:'))
+
+tổng = n1+n2+n3
+tích = n1*n2*n3
+trung = tổng/3
+
+print('tổng là:' , tổng)
+print('tích là:' , tích)
+print('trung bình là:' ,  trung)
