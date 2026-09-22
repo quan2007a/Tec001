df9de76299b08a616214c3e3bb98ef5553f00ba5
Quân <nnquan2000@gmail.com>
2026-09-22 09:46:57 +0700

    xong bài

---
+++ Assigment_1/exercise7.py
@@ -0,0 +1,14 @@
+import random
+
+digit1 = random.randint(0, 9)
+digit2 = random.randint(0, 9)
+digit3 = random.randint(0, 9)
+
+print(digit1, digit2, digit3, sep="") 
+
+digit4 = random.randint(1, 6)
+digit5 = random.randint(1, 6)
+digit6 = random.randint(1, 6)
+digit7 = random.randint(1, 6)
+
+print(digit4, digit5, digit6, digit7, sep="")
